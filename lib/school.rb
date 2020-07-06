c6dc93d877ba7de5635b5103128e314ddb8b54f8
zachary mines <zmines@wesleyan.edu>
2020-07-06 18:45:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School 
+  def initialize(school)
+    @school = school 
+    @roster = {}
+  end 
+  
+  attr_accessor :school 
+  attr_reader :roster
+  
+  
+  def add_student(student, grade)
+    if !@roster.include?(grade)
+      @roster[grade] = [student] 
+    else 
+      @roster[grade] << student
+    end  
+    @roster 
+  end 
+
+  def grade (grd)
+    @roster[grd]
+  end 
+  def sort
+    a = @roster.sort_by{|k,v| k}
+    @roster = {}
+    a.each do |a,b|
+      @roster[a] = b.sort 
+    end 
+    @roster
+  end 
+end 
