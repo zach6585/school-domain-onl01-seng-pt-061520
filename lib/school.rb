fe3a7e1d434916de3b098aa987a2319d40792753
zachary mines <zmines@wesleyan.edu>
2020-07-06 18:39:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School 
+  def initialize(school)
+    @school = school 
+  end 
+  
+  def roster=(roster)
+    @roster = roster
+  end 
+  
+  def roster
+    roster
+  end 
+  
+  
+  def add_student(student, grade)
+    if !@roster.include?(grade)
+      @roster[grade] = [student] 
+    else 
+      @roster[grade] << student
+    end  
+    @roster 
+  end 
+
+  def grade (grd)
+    @roster[grd]
+  end 
+  def sort
+    @roster.each do |k,v|
+      v = v.sort 
+    end 
+    @roster 
+  end 
+end 
